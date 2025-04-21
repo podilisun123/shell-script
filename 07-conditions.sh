b577bbde5c16e72c71b6ee4156f9f0c5f0ca20f3
sundar <sudar@gmail.com>
2025-04-21 20:17:31 +0530

    variables

---
+++ 07-conditions.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+read -p "enter number1:" NUMBER1
+
+if [ $NUMBER1 -ge 10 ]
+then 
+    echo "your entered number is ${NUMBER1} greater than 10 "
+else
+    echo "your entered number is ${NUMBER1} less than 10"
