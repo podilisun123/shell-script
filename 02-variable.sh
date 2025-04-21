82112eb0c8158fc298a3d8151d54ac733274c150
sundar <sudar@gmail.com>
2025-04-21 14:07:24 +0530

    first shell script

--- 02-variable.sh
+++ 02-variable.sh
@@ -2,7 +2,7 @@
 PERSON1=Ravi
 PERSON2=JAGGU
 
-echo "${PERSON1}: hi ${PERSON2} ,how are your"
+echo "${PERSON1}: hi ${PERSON2} ,how are you"
 echo "${PERSON2}: hi ${PERSON1}, i am fine and how are you"
 echo "${PERSON1}: how things are going ${PERSON2}"
 echo "${PERSON2}: just now i am started devops ${PERSON1}"
---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Please enter two numbers"
+NUMBER1=$1
+NUMBER2=$2
+SUM=$(($NUMBER1 + $NUMBER2))
+
+echo "$NUMBER1 AND $NUMBER2 SUM IS ${SUM}"
+
