2722e98ea8ab8b1624f52980f816cd382f5e2db2
sundar <sudar@gmail.com>
2025-04-21 21:19:32 +0530

    variables

--- 09-functions.sh
+++ 09-functions.sh
@@ -1,5 +1,13 @@
 USER=$(id -u)
+TIME_STAMP=$(date +date +%F-%H-%M-%S)
 SCRIPT_FILE=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp-${SCRIPT_FILE}-${TIME_STAMP}.log
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
 
 if [ $USER -ne 0 ]
 then
@@ -8,5 +16,17 @@ then
 else
     echo "you are super user"
 fi
+validate(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e " $2 ...$R Failure $N"
+    else 
+        echo -e "$2 ...$G Success $N"
+    fi
+}
 
-dnf install mysql -y
+dnf install mysql -y &>>LOGFILE
+validate $? "installing mysql"
+
+dnf install git -y &>>LOGFILE
+validate $? "installing git"
