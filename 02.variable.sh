c628cc70f95f6fb9c06bfe027851a4e36cc0668c
sundar <sudar@gmail.com>
2025-04-21 14:02:00 +0530

    first shell script

--- 02.variable.sh
+++ 02.variable.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+PERSON1=Ravi
+PERSON2=JAGGU
 
-echo "Sundar: hi Rajesh ,how are your"
-echo "Rajesh: hi sundar, i am fine and how are you"
-echo "Sundar: how things are going Rajesh"
-echo "Rajesh: just now i am started devops"
+echo "${PERSON1}: hi ${PERSON2} ,how are your"
+echo "${PERSON2}: hi ${PERSON1}, i am fine and how are you"
+echo "${PERSON1}: how things are going ${PERSON2}"
+echo "${PERSON2}: just now i am started devops ${PERSON1}"
