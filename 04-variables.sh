1914afa82045776b867f6579a34505abb506db2e
sundar <sudar@gmail.com>
2025-04-21 19:45:49 +0530

    variables

--- 04-variables.sh
+++ 04-variables.sh
@@ -3,4 +3,4 @@ echo "enter username:"
 read UNAME
 echo "enter password:"
 read -s PASSWD
-echo "The username is ${UNAME} and password is ${PASSWD}
+echo "The username is ${UNAME} and password is ${PASSWD}"
