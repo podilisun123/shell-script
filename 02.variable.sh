f33b80ca58322b11bfa1d9e845133b3a1587c314
sundar <sudar@gmail.com>
2025-04-21 13:59:17 +0530

    first shell script

--- 02.variable.sh
+++ 02.variable.sh
@@ -3,4 +3,4 @@
 echo "Sundar: hi Rajesh ,how are your"
 echo "Rajesh: hi sundar, i am fine and how are you"
 echo "Sundar: how things are going Rajesh"
-echo "Rajesh: just now i am started devops:
+echo "Rajesh: just now i am started devops"
